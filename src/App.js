5e83e02b7819cd26e80ea72f3ecc39a708bf1970
Gabriel Gerardo Pisonero López <gabriel.pisonero@campusucc.edu.co>
2024-05-26 19:05:45 -0500

    Creacion de contadores para los Todos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
 
 const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
@@ -12,10 +13,26 @@ const defaultTodos = [
 ];
 
 function App() {
+
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  console.log(searchValue);
+
   return (
     <>
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter
+        completed={completedTodos} 
+        total={totalTodos}  
+      />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+        
 
       <TodoList>
         {defaultTodos.map(todo => (
