8fb315007d1f01cc5336ae8a92316f510f64817a
Gabriel Gerardo Pisonero López <gabriel.pisonero@campusucc.edu.co>
2024-05-28 21:51:37 -0500

    Agregando filtro para buscador a los todos

--- src/App.js
+++ src/App.js
@@ -20,6 +20,14 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue .toLowerCase();
+      return todoText.includes(searchText)
+    }
+  )
+
   console.log(searchValue);
 
   return (
@@ -35,7 +43,7 @@ function App() {
         
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
